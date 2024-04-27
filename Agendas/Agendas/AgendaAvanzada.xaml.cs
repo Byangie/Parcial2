eb8090622fc2e96182f847678c1c52fba720a461
Fazt <contact@faztweb.com>
2024-04-27 11:01:46 -0600

    examen final

---
+++ Agendas/Agendas/AgendaAvanzada.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Agendas
+{
+    /// <summary>
+    /// Lógica de interacción para AgendaAvanzada.xaml
+    /// </summary>
+    public partial class AgendaAvanzada : Window
+    {
+        public AgendaAvanzada()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
